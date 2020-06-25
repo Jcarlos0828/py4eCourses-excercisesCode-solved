414bcf90735d4e35d36ce4bdb65e917115afeb7c
José Carlos Del Castillo <jose.carlos4013@gmail.com>
2020-06-25 12:56:12 -0500

    Documented excercise RetrieveData_Sockets.py

--- Using Python to access Web Data/Week 3/RetrieveData_Sockets.py
+++ Using Python to access Web Data/Week 3/RetrieveData_Sockets.py
@@ -1,3 +1,12 @@
+#Code Author: José Carlos del Castillo Estrada
+
+#Excercise solved from the book "Python for Everybody" by Dr. Charles R. Severance
+#Following the Coursera program "Using Python to access Web Data" by the University of Michigan
+
+'''
+Extrieve data with the HTTP protocol using sockets and print it
+'''
+
 import socket
 #This makes the connection
 mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -6,6 +15,7 @@ cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mySock.send(cmd)
 
 while True:
+    #The number is = to the # of chars to recieve 
     data = mySock.recv(612)
     if (len(data) < 1):
         break
