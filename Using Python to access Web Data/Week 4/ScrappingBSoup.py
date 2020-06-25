5326a52231bf1fff4655e1e5ec519dd1f3a39489
José Carlos Del Castillo <jose.carlos4013@gmail.com>
2020-06-25 15:29:34 -0500

    Added the URL as a comment in ScrappingBSoup.py

--- Using Python to access Web Data/Week 4/ScrappingBSoup.py
+++ Using Python to access Web Data/Week 4/ScrappingBSoup.py
@@ -17,6 +17,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# http://py4e-data.dr-chuck.net/comments_707321.html
 url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
