e6a34083505521f3c19526f0374fc722051a905f
José Carlos Del Castillo <jose.carlos4013@gmail.com>
2020-06-24 22:03:24 -0500

    Solved the excercise

--- Using Python to access Web Data/Week 2/ExtractingData_RE.py
+++ Using Python to access Web Data/Week 2/ExtractingData_RE.py
@@ -1,8 +1,23 @@
+#Code Author: José Carlos del Castillo Estrada
+
+#Excercise solved from the book "Python for Everybody" by Dr. Charles R. Severance
+#Following the Coursera program "Using Python to access Web Data" by the University of Michigan
+
+'''
+Program to find all numbers in a .txt using Regular Expresssions and sum them
+'''
 import re
-text = open('regex_sum42.txt')
+#Insert your path (for windows works)
+#text = open(r'C:\users\Path\file\Using Python to access Web Data\Week 2\regex_sum42.txt')
+text = open('regex_sum707319.txt')
 sentence = list()
+acum = 0
 for i in text:
     i.rstrip()
-    coinc = re.findall('[0-9]', i)
+    coinc = re.findall('([0-9]+)', i)
+    if len(coinc) < 1 : 
+        continue
+    for j in coinc:
+        acum = acum + int(j)
     sentence.append(coinc)
-print(sentence)
+print("Acum:",acum)
